a8c182b5a4277e95fab2c13d12dbca6e02c723df
abhijitRayarao <abhijitrayarao@gmail.com>
2025-05-16 10:59:25 +0530

    16/05/2025

--- src/components/BlogItem.tsx
+++ src/components/BlogItem.tsx
@@ -1,31 +1,15 @@
 import type { Post } from "../types/post";
 
 const BlogItem = ({ post }: { post: Post }) => {
-  const { title, content, image, tags } = post;
+  const { title, content } = post;
 
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
-      <figure>
-        <img
-          src={image}
-          alt={title}
-        />
-      </figure>
       <div className="card-body">
         <h2 className="card-title">
           {title}
-          {tags.length > 0 && (
-            <div className="badge badge-secondary">{tags[0]}</div>
-          )}
         </h2>
         <p>{content.split('\n')[0]}</p>
-        <div className="card-actions justify-end">
-          {tags.map((tag: string, index: number) => (
-            <div key={index} className="badge badge-outline">
-              {tag}
-            </div>
-          ))}
-        </div>
       </div>
     </div>
   );
